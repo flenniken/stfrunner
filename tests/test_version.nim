85ea787c457067844df0a482e8bc76de0738b593
Steve Flenniken <steve.flenniken@gmail.com>
2025-11-28 12:27:42 -0800

    Remove unused matches module.

--- tests/test_version.nim
+++ tests/test_version.nim
@@ -1,15 +1,18 @@
 import std/unittest
-import matches
 import version
 import options
+import regexes
+
+const
+  versionPattern = r"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$"
+  ## Stfrunner version regular expression pattern.  It has
+  ## three components each with one to three digits: i.e. 1.2.3,
+  ## 123.456.789, 0.1.0,... .
 
 suite "version.nim":
 
-  test "test me":
-    check 1 == 1
-
   test "test version string":
-    let matchesO = matchVersion(stfrunnerVersion)
+    let matchesO = matchPattern(stfrunnerVersion, versionPattern, 0, 3)
     if not matchesO.isSome:
-      echo "Invalid StaticTea version number: " & stfrunnerVersion
+      echo "Invalid stfrunner version number: " & stfrunnerVersion
       fail
