2b88904a25e47ef64a144979a793c759e988e9db
Steve Flenniken <steve.flenniken@gmail.com>
2025-12-06 15:02:02 -0800

    Switch from re to nre to remove the dependency on the Perl shared library.

--- src/regexes.nim
+++ src/regexes.nim
@@ -1,78 +1,6 @@
-## 
-#$ Perl regular expression matching.
-#$
-#$ Examples:
-#$
-#$ Match a string with "abc" in it:
-#$
-#$ ~~~nim
-#$ let line = "123abc456"
-#$ let pattern = ".*abc"
-#$ let matchesO = matchPattern(line, pattern, start=0, numGroups=0)
-#$
-#$ check matchesO.isSome == true
-#$ check matchesO.get().length == 6
-#$ ~~~
-#$
-#$ Match a file and line number like: filename(234):
-#$
-#$ ~~~ nim
-#$ let line = "template.html(87)"
-#$ let pattern = r"^(.*)\(([0-9]+)\):$"
-#$ let matchesO = matchPattern(line, pattern, start = 0, groups = 2)
-#$
-#$ check matchesO.isSome == true
-#$ let (filename, lineNum, length) = matchesO.get2GroupsLen()
-#$ check filename == "template.html"
-#$ check lineNum == "87"
-#$ check length == 14
-#$ ~~~
-#$
-#$ Replace the patterns in the string with their replacements:
-#$
-#$ ~~~ nim
-#$ var replacements: seq[Replacement]
-#$ replacements.add(newReplacement("abc", "456"))
-#$ replacements.add(newReplacement("def", ""))
-#$
-#$ let resultStringO = replaceMany("abcdefabc", replacements)
-#$
-#$ check resultStringO.isSome
-#$ check resultStringO.get() == "456456"
-#$ ~~~
+## Regular expression matching.
 
-# The notice below is there because this module includes the re module.
-#[
-Written by Philip Hazel
-Copyright (c) 1997-2005 University of Cambridge
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-  Redistributions of source code must retain the above copyright
-  notice, this list of conditions and the following disclaimer.
-  Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution.
-  Neither the name of the University of Cambridge nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]#
-
-import std/re
+import std/nre
 import std/options
 
 const
@@ -162,22 +90,30 @@ func matchRegex*(str: string, regex: CompiledPattern, start: Natural,
     numGroups: Natural): Option[Matches] =
   ## Match a regular expression pattern in a string. Start is the
   ## index in the string to start the search. NumGroups is the number
-  ## of groups in the pattern.
+  ## of groups in the pattern. The pattern is anchored.
   if start >= str.len:
     return
   if numGroups > maxGroups:
     return
 
-  var maxGroups = newSeq[string](maxGroups)
-  let length = matchLen(str, regex, maxGroups, start)
-  if length != -1:
-    if numGroups == 0:
-      result = some(newMatches(length, start))
-    else:
-      var groups: seq[string]
-      for ix in 0 .. numGroups-1:
-        groups.add(maxGroups[ix])
-      result = some(newMatches(length, start, groups))
+  # Use anchored matching: check if pattern matches from start position
+  if start < str.len:
+    let substr = str[start .. ^1]
+    let matchResult = match(substr, regex)
+    if matchResult.isSome:
+      let m = matchResult.get()
+      let matchStr = m.match
+      let matchLength = matchStr.len
+      if numGroups == 0:
+        result = some(newMatches(matchLength, start))
+      else:
+        var groups: seq[string]
+        for ix in 0 .. numGroups-1:
+          try:
+            groups.add(m.captures[ix])
+          except:
+            groups.add("")
+        result = some(newMatches(matchLength, start, groups))
 
 func compilePattern*(pattern: string): Option[CompiledPattern] =
   ## Compile the pattern and return a regex object.
@@ -192,9 +128,10 @@ func matchPattern*(str: string, pattern: string,
     start: Natural, numGroups: Natural): Option[Matches] =
   ## Match a regular expression pattern in a string. Start is the
   ## index in the string to start the search. NumGroups is the number
-  ## of groups in the pattern.
-  ##
-  ## Note: the pattern uses the anchored option.
+  ## of groups in the pattern. The pattern is anchored.
+  # If pattern starts with ^ (beginning anchor) and start > 0, it cannot match
+  if pattern.len > 0 and pattern[0] == '^' and start > 0:
+    return
   let regexO = compilePattern(pattern)
   if not regexO.isSome:
     return
@@ -207,11 +144,11 @@ func newReplacement*(pattern: string, sub: string): Replacement =
 proc replaceMany*(str: string, replacements: seq[Replacement]): Option[string] =
   ## Replace the patterns in the string with their replacements.
 
-  var subs: seq[tuple[pattern: CompiledPattern, repl: string]]
+  var resultStr = str
   for r in replacements:
     let regexO = compilePattern(r.pattern)
     if not regexO.isSome:
       return
     let regex = regexO.get()
-    subs.add((regex, r.sub))
-  result = some(multiReplace(str, subs))
+    resultStr = replace(resultStr, regex, r.sub)
+  result = some(resultStr)
